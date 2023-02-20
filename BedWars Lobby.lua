2b957f94c501b47b5178b2f04d35188f1ee73b7f
757788428949485651495849884358443235871 <123597687+757788428949485651495849884358443235871@users.noreply.github.com>
2023-02-20 15:42:20 -0500

    Update BedWars Lobby.lua

--- BedWars Lobby.lua
+++ BedWars Lobby.lua
@@ -1,30 +1,30 @@
-   local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
-   local Window = Rayfield:CreateWindow({
-      Name = "Destroy UI Elements [PRIVATE] | BedWars 🔥 [SEASON 7!]",
-      LoadingTitle = "Destroy UI Elements [PRIVATE] | BedWars Lobby",
-      LoadingSubtitle = "Developed By Corrade#4385",
-      ConfigurationSaving = {
-         Enabled = true,
-         FolderName = "Destroy UI Elements [PRIVATE]",
-         FileName = "BedWars Lobby"
-      },
-      Discord = {
-         Enabled = false,
-         Invite = "SIRIUS",
-         RememberJoins = true
-      },
-      KeySystem = false,
-      KeySettings = {
-         Title = "Sirius Hub",
-         Subtitle = "Key System",
-         Note = "Join the discord (discord.gg/sirius)",
-         FileName = "SiriusKey",
-         SaveKey = true,
-         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-         Key = "Hello"
-      }
-   })
-   local Tab = Window:CreateTab("PlayerGui", 4483362458)
-   
-   Rayfield:LoadConfiguration()
+local Window = Rayfield:CreateWindow({
+   Name = "Destroy UI Elements [PRIVATE] | BedWars 🔥 [SEASON 7!]",
+   LoadingTitle = "Destroy UI Elements [PRIVATE] | BedWars Lobby",
+   LoadingSubtitle = "Developed By Corrade#4385",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "Destroy UI Elements [PRIVATE]",
+      FileName = "BedWars Lobby"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "SIRIUS",
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "Destroy UI Elements [PRIVATE]",
+      Subtitle = "Key System",
+      Note = nil,
+      FileName = "Destroy UI Elements Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = "6a9bd495ac5086698769f8634cf"
+   }
+})
+local Tab = Window:CreateTab("PlayerGui", 4483362458)
+
+Rayfield:LoadConfiguration()
