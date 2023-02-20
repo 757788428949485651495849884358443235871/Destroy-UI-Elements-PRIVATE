9ca4369430310eda1cf304ceae123ba701047c5d
757788428949485651495849884358443235871 <123597687+757788428949485651495849884358443235871@users.noreply.github.com>
2023-02-20 18:43:38 -0500

    Create Universal.lua

---
+++ Universal.lua
@@ -0,0 +1,70 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+Rayfield:Notify({
+   Title = "Game Not Found",
+   Content = "Supported Game Not Found",
+   Duration = 6.5,
+   Image = 4483362458,
+   Actions = {
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+      end
+   },
+},
+})
+-- Destroy UI Elements Window
+local Window = Rayfield:CreateWindow({
+   Name = "Destroy UI Elements [PRIVATE] | Universal",
+   LoadingTitle = "Destroy UI Elements [PRIVATE] | Universal",
+   LoadingSubtitle = "Developed By Corrade#4385",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "Destroy UI Elements [PRIVATE]",
+      FileName = "BedWars Lobby"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "SIRIUS",
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "Destroy UI Elements [PRIVATE]",
+      Subtitle = "Key System",
+      Note = nil,
+      FileName = "Destroy UI Elements Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = "6a9bd495ac5086698769f8634cf"
+   }
+})
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local Section = Tab:CreateSection("Chat")
+
+local HideChatToggle = Tab:CreateToggle({
+   Name = "Hide Chat",
+   CurrentValue = false,
+   Flag = "Chat",
+   Callback = function(Value)
+       if Value then
+         game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+       else
+         game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
+       end
+   end,
+})
+local HideBubbleChatToggle = Tab:CreateToggle({
+   Name = "Hide Bubble Chat",
+   CurrentValue = false,
+   Flag = "BubbleChat",
+   Callback = function(Value)
+       if Value then
+         game.Chat.BubbleChatEnabled = false
+       else
+         game.Chat.BubbleChatEnabled = true
+       end
+   end,
+})
+Rayfield:LoadConfiguration()
