006227e3ccd222e92d4a2bf093aa9b107e235b1c
757788428949485651495849884358443235871 <123597687+757788428949485651495849884358443235871@users.noreply.github.com>
2023-02-20 15:41:50 -0500

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -1,5 +1,19 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
+Rayfield:Notify({
+    Title = "Game Found",
+    Content = "Supported Game Found: Arsenal",
+    Duration = 6.5,
+    Image = 4483362458,
+    Actions = {
+       Ignore = {
+          Name = "Okay!",
+          Callback = function()
+       end
+    },
+ },
+ })
+ -- Destroy UI Elements Window
    local Window = Rayfield:CreateWindow({
       Name = "Destroy UI Elements [PRIVATE] | Arsenal",
       LoadingTitle = "Destroy UI Elements [PRIVATE] | Arsenal",
@@ -14,21 +28,23 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shle
          Invite = "SIRIUS",
          RememberJoins = true
       },
-      KeySystem = false,
+      KeySystem = true,
       KeySettings = {
-         Title = "Sirius Hub",
+         Title = "Destroy UI Elements [PRIVATE]",
          Subtitle = "Key System",
-         Note = "Join the discord (discord.gg/sirius)",
-         FileName = "SiriusKey",
+         Note = nil,
+         FileName = "Destroy UI Elements Key",
          SaveKey = true,
-         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-         Key = "Hello"
+         GrabKeyFromSite = false,
+         Key = "6a9bd495ac5086698769f8634cf"
       }
    })
-   local Tab = Window:CreateTab("PlayerGui", 4483362458)
+   local Tab = Window:CreateTab("Main", 4483362458)
+
+-- Main Tab
 
    local HealthBarToggle = Tab:CreateToggle({
-      Name = "Destroy Health Bar",
+      Name = "Hide Health Bar",
       CurrentValue = false,
       Flag = "HealthBar",
       Callback = function(Value)
@@ -39,34 +55,80 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shle
           end
       end,
   })
-   Rayfield:LoadConfiguration() 
---[[
-   local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+  local HideStreakButton = Tab:CreateButton({
+    Name = "Hide Streak",
+    Callback = function()
+        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Interface.Vitals.Streak:Destroy()
+    end,
+ })
+ local Tab = Window:CreateTab("Themes", 4483362458)
 
-   local Window = Rayfield:CreateWindow({
-      Name = "Destroy UI Elements [PRIVATE] | BedWars 🔥 [SEASON 7!]",
-      LoadingTitle = "Destroy UI Elements [PRIVATE] | BedWars Lobby",
-      LoadingSubtitle = "Developed By Corrade#4385",
-      ConfigurationSaving = {
-         Enabled = true,
-         FolderName = "Destroy UI Elements [PRIVATE]",
-         FileName = "BedWars Lobby"
-      },
-      Discord = {
-         Enabled = false,
-         Invite = "SIRIUS",
-         RememberJoins = true
-      },
-      KeySystem = false,
-      KeySettings = {
-         Title = "Sirius Hub",
-         Subtitle = "Key System",
-         Note = "Join the discord (discord.gg/sirius)",
-         FileName = "SiriusKey",
-         SaveKey = true,
-         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-         Key = "Hello"
-      }
-   })
-   local Tab = Window:CreateTab("PlayerGui", 4483362458)
-   Rayfield:LoadConfiguration()--]]
+ -- Themes Tab
+
+ local HalloweenThemeToggle = Tab:CreateToggle({
+    Name = "Halloween Theme",
+    CurrentValue = false,
+    Flag = "Halloween",
+    Callback = function(Value)
+        if Value then
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.HWOverlay.Visible = true
+        else
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.HWOverlay.Visible = false
+        end
+    end,
+})
+local ChristmasThemeToggle = Tab:CreateToggle({
+    Name = "Christmas Theme",
+    CurrentValue = false,
+    Flag = "Christmas",
+    Callback = function(Value)
+        if Value then
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.Holiday.Visible = true
+        else
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.Holiday.Visible = false
+        end
+    end,
+})
+local StPatrickDayToggle = Tab:CreateToggle({
+    Name = "St. Patrick's Day Theme",
+    CurrentValue = false,
+    Flag = "St. Patrick's Day",
+    Callback = function(Value)
+        if Value then
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.CloversL.Visible = true
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.PatrickShine.Visible = true
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.CloversR.Visible = true
+        else
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.CloversL.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.PatrickShine.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.CloversR.Visible = false
+        end
+    end,
+})
+local Tab = Window:CreateTab("Main Menu", 4483362458)
+
+-- Main Menu Tab
+
+local HalloweenThemeToggle = Tab:CreateToggle({
+    Name = "Hide Promotions",
+    CurrentValue = false,
+    Flag = "Promotions",
+    Callback = function(Value)
+        if Value then
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.Promotions.Visible = false
+        else
+            game:GetService("Players").LocalPlayer.PlayerGui.Menew.Main.Promotions.Visible = true
+        end
+    end,
+})
+local Tab = Window:CreateTab("Miscellaneous", 4483362458)
+
+-- Miscellaneous Tab
+
+  local DestroyButton = Tab:CreateButton({
+   Name = "Destroy",
+   Callback = function()
+      Rayfield:Destroy()
+   end,
+})
+Rayfield:LoadConfiguration()
