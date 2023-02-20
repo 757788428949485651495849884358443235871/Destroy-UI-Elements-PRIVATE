e9a5da9d797a8db513b09c9e3d1ac0ccd53f5910
757788428949485651495849884358443235871 <123597687+757788428949485651495849884358443235871@users.noreply.github.com>
2023-02-20 18:27:36 -0500

    Update BedWars Lobby.lua

--- BedWars Lobby.lua
+++ BedWars Lobby.lua
@@ -1,5 +1,19 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
+Rayfield:Notify({
+   Title = "Game Found",
+   Content = "Supported Game Found: BedWars Lobby",
+   Duration = 6.5,
+   Image = 4483362458,
+   Actions = {
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+      end
+   },
+},
+})
+-- Destroy UI Elements Window
 local Window = Rayfield:CreateWindow({
    Name = "Destroy UI Elements [PRIVATE] | BedWars 🔥 [SEASON 7!]",
    LoadingTitle = "Destroy UI Elements [PRIVATE] | BedWars Lobby",
@@ -25,6 +39,73 @@ local Window = Rayfield:CreateWindow({
       Key = "6a9bd495ac5086698769f8634cf"
    }
 })
-local Tab = Window:CreateTab("PlayerGui", 4483362458)
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local Section = Tab:CreateSection("Chat")
+
+local HideChatToggle = Tab:CreateToggle({
+   Name = "Hide Chat",
+   CurrentValue = false,
+   Flag = "Chat",
+   Callback = function(Value)
+       if Value then
+         game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+       else
+         game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
+       end
+   end,
+})
+local HideBubbleChatToggle = Tab:CreateToggle({
+   Name = "Hide Bubble Chat",
+   CurrentValue = false,
+   Flag = "BubbleChat",
+   Callback = function(Value)
+       if Value then
+         game.Chat.BubbleChatEnabled = false
+       else
+         game.Chat.BubbleChatEnabled = true
+       end
+   end,
+})
+local Section = Tab:CreateSection("UI Elements")
+
+local HideLeaderBoardToggle = Tab:CreateToggle({
+   Name = "Hide LeaderBoard",
+   CurrentValue = false,
+   Flag = "LeaderBoard",
+   Callback = function(Value)
+       if Value then
+         game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+       else
+         game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, true)
+       end
+   end,
+})
+local TopBarToggle = Tab:CreateToggle({
+   Name = "Hide TopBar",
+   CurrentValue = false,
+   Flag = "TopBar",
+   Callback = function(Value)
+       if Value then
+         game:GetService("Players").LocalPlayer.PlayerGui.TopBarAppGui.TopBarApp.Visible = false
+       else
+         game:GetService("Players").LocalPlayer.PlayerGui.TopBarAppGui.TopBarApp.Visible = true
+       end
+   end,
+})
+local EmoteUIToggle = Tab:CreateToggle({
+   Name = "Hide Emote UI",
+   CurrentValue = false,
+   Flag = "EmoteUI",
+   Callback = function(Value)
+       if Value then
+         local StarterGui = game:GetService("StarterGui")
+         StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
+       else
+         local StarterGui = game:GetService("StarterGui")
+         StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, true)
+       end
+   end,
+})
 
 Rayfield:LoadConfiguration()
